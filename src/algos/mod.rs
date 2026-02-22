dfa151533e6452cbdfc7087b001578253930de12
Abhinav <satsyndicate@gmail.com>
2026-02-23 00:04:04 +0530

    changing directories

---
+++ src/algos/mod.rs
@@ -0,0 +1,9 @@
+mod binary_search;
+mod bubble_sort;
+mod counting_sort;
+mod heap_sort;
+mod insertion_sort;
+mod merge_sort;
+mod quick_sort;
+mod selection_sort;
+mod subsequence_sum;
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
-mod binary_search;
-mod bubble_sort;
-mod counting_sort;
-mod heap_sort;
-mod insertion_sort;
-pub mod merge_sort;
-mod quick_sort;
-mod selection_sort;
-mod subsequence_sum;
+mod algos;
