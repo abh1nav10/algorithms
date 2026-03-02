ce9cacd0f63d238c5e0eebe6f78130b9a163ca8f
Abhinav <satsyndicate@gmail.com>
2026-03-02 15:02:37 +0530

    Implement Kahn's algorithm to detect cycles

--- src/data_structures/graph.rs
+++ src/data_structures/graph.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 //! A directed-weighted graph!
 
 use std::collections::{HashMap, HashSet, VecDeque};
@@ -10,11 +12,8 @@ struct Node<T, W> {
 }
 
 impl<T, W: Ord> Node<T, W> {
-    fn new(value: T, edges: Vec<(usize, W)>) -> Self {
-        Self {
-            value: Rc::new(value),
-            edges,
-        }
+    fn new(value: Rc<T>, edges: Vec<(usize, W)>) -> Self {
+        Self { value, edges }
     }
 }
 
@@ -45,18 +44,22 @@ where
         }
     }
 
-    pub fn insert_node(&mut self, value: T, edges: Vec<(Rc<T>, W)>) {
+    pub fn insert_node(&mut self, value: Rc<T>, edges: Vec<(Rc<T>, W)>) {
         let mut edge = Vec::with_capacity(edges.len());
         for element in edges {
             if let Some(index) = self.list.get(&element.0) {
                 edge.push((*index, element.1));
             }
         }
-        let node = Node::new(value, edge);
-        self.nodes.push(node);
-        let index = self.nodes.len() - 1;
-        let cloned = Rc::clone(&self.nodes[index].value);
-        self.list.insert(cloned, index);
+        if let Some(element) = self.list.get(&value) {
+            self.nodes[*element].edges = edge;
+        } else {
+            let node = Node::new(value, edge);
+            self.nodes.push(node);
+            let index = self.nodes.len() - 1;
+            let cloned = Rc::clone(&self.nodes[index].value);
+            self.list.insert(cloned, index);
+        }
     }
 
     pub fn connect(&mut self, connect: Rc<T>, to: Rc<T>, weight: W) -> Result<(), &str> {
@@ -132,8 +135,71 @@ where
         Err("No path connects the source node to the destination node!")
     }
 
+    // Naive but working attempt!
+    //
+    // pub fn has_cycle(&self) -> bool {
+    //     let mut map = HashMap::new();
+    //     for (key, value) in self.list.iter() {
+    //         let key = Rc::clone(key);
+    //         map.entry(key).or_insert(0);
+    //         for (index, _) in self.nodes[*value].edges.iter() {
+    //             let element = Rc::clone(&self.nodes[*index].value);
+    //             map.entry(element).and_modify(|e| *e += 1).or_insert(1);
+    //         }
+    //     }
+    //     let mut elements = Vec::with_capacity(self.list.len());
+    //     for (key, value) in map.iter() {
+    //         if *value == 0 {
+    //             let key = Rc::clone(key);
+    //             elements.push(key);
+    //         }
+    //     }
+    //     let mut count = 0;
+    //     while let Some(element) = elements.pop() {
+    //         count += 1;
+    //         let node = self
+    //             .list
+    //             .get(&element)
+    //             .expect("It has already been inserted into the map!");
+    //         for (i, _) in self.nodes[*node].edges.iter() {
+    //             let rc = Rc::clone(&self.nodes[*i].value);
+    //             let index = map.get_mut(&rc).expect("Has to be there");
+    //             *index -= 1;
+    //             if *index == 0 {
+    //                 elements.push(rc);
+    //             }
+    //         }
+    //     }
+    //     count != self.list.len()
+    // }
+
+    /// Implementation of Kahn's algorithm.
     pub fn has_cycle(&self) -> bool {
-        todo!()
+        let len = self.nodes.len();
+        let mut vector = vec![0; len];
+        for element in self.nodes.iter() {
+            for (e, _) in element.edges.iter() {
+                vector[*e] += 1;
+            }
+        }
+        let mut zero = Vec::with_capacity(len);
+        for (index, element) in vector.iter().enumerate() {
+            if *element == 0 {
+                zero.push(index);
+            }
+        }
+        let mut count = 0;
+        while let Some(element) = zero.pop() {
+            count += 1;
+            for (i, _) in self.nodes[element].edges.iter() {
+                let i = *i;
+                vector[i] -= 1;
+                if vector[i] == 0 {
+                    zero.push(i);
+                }
+            }
+        }
+        count != len
     }
 
     pub fn remove_node(&self, node: Rc<T>) -> Result<(), &str> {
@@ -142,5 +208,27 @@ where
 
     pub fn dijkstra_shortest_path(&self, source: Rc<T>, dest: Rc<T>) {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_cycle() {
+        let mut graph = Graph::new();
+        let n1 = Rc::new(1);
+        let n2 = Rc::new(2);
+        let n3 = Rc::new(3);
+        graph.insert_node(Rc::clone(&n3), vec![]);
+        graph.insert_node(Rc::clone(&n2), vec![(Rc::clone(&n3), 0)]);
+        graph.insert_node(Rc::clone(&n1), vec![(Rc::clone(&n2), 0)]);
+
+        assert!(!graph.has_cycle());
+
+        graph.connect(Rc::clone(&n3), Rc::clone(&n1), 0);
+
+        assert!(graph.has_cycle());
     }
 }
